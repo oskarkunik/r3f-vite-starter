6866c328220585343de18cac0333193225810908
Oskar Kunik <oskar.kunik@gmail.com>
2023-11-14 20:23:07 +0100

    Add hdri as envmap

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,13 +1,14 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Environment } from "@react-three/drei";
 import React from "react";
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
+      <OrbitControls enableDamping dampingFactor={0.05} />
       <mesh>
+        <Environment files="src/assets/hdri/env.hdr" />
         <sphereGeometry args={[5, 10, 10]} />
-        <meshBasicMaterial color={"#ff0000"} />
+        <meshStandardMaterial roughness={0} metalness={1} />
       </mesh>
     </>
   );
