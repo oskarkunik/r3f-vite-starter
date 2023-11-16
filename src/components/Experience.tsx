87f52a288d2a9b5617a8ab3304ba359b51a5102a
Oskar Kunik <oskar.kunik@gmail.com>
2023-11-16 17:45:21 +0100

    Fix tile positions

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,12 +3,17 @@ import React, { useEffect, useState } from "react";
 import { BoxGeometry, BufferGeometry, CylinderGeometry, Vector2 } from "three";
 import { mergeGeometries } from "three/examples/jsm/utils/BufferGeometryUtils.js";
 
+const HEX_GRID_SIZE = 10;
+
 const createHexGeometry = (height: number, position: Vector2) =>
   new CylinderGeometry(1, 1, height, 6, 1, false).translate(
     position.x,
     height * 0.5,
     position.y
   );
+
+const tileToPosition = (tileX: number, tileY: number) =>
+  new Vector2((tileX + (tileY % 2) * 0.5) * 1.77, tileY * 1.535);
 
 export const Experience = () => {
   const [hexagonGeometries, setHexagonGeometries] = useState<BufferGeometry>(
@@ -21,7 +26,11 @@ export const Experience = () => {
     );
 
   useEffect(() => {
-    makeHex(5, new Vector2(0, 0));
+    new Array(HEX_GRID_SIZE).fill(0).forEach((row, rowIndex) => {
+      new Array(HEX_GRID_SIZE).fill(0).forEach((column, columnIndex) => {
+        makeHex(1, tileToPosition(rowIndex, columnIndex));
+      });
+    });
   }, []);
 
   return (
