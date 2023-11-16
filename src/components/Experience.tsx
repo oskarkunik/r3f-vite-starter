560854378daeb02d98b7e3d4d434f70894148dcb
Oskar Kunik <oskar.kunik@gmail.com>
2023-11-16 17:26:24 +0100

    Generate hexagon

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,14 +1,35 @@
 import { OrbitControls, Environment } from "@react-three/drei";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BoxGeometry, BufferGeometry, CylinderGeometry, Vector2 } from "three";
+import { mergeGeometries } from "three/examples/jsm/utils/BufferGeometryUtils.js";
+
+const createHexGeometry = (height: number, position: Vector2) =>
+  new CylinderGeometry(1, 1, height, 6, 1, false).translate(
+    position.x,
+    height * 0.5,
+    position.y
+  );
 
 export const Experience = () => {
+  const [hexagonGeometries, setHexagonGeometries] = useState<BufferGeometry>(
+    new BoxGeometry(0, 0, 0)
+  );
+
+  const makeHex = (height: number, position: Vector2) =>
+    setHexagonGeometries((current) =>
+      mergeGeometries([current, createHexGeometry(height, position)])
+    );
+
+  useEffect(() => {
+    makeHex(1, new Vector2(0, 0));
+  }, []);
+
   return (
     <>
       <OrbitControls enableDamping dampingFactor={0.05} />
-      <mesh>
+      <mesh geometry={hexagonGeometries}>
         <Environment files="src/assets/hdri/env.hdr" />
-        <sphereGeometry args={[5, 10, 10]} />
-        <meshStandardMaterial roughness={0} metalness={1} />
+        <meshStandardMaterial roughness={0} metalness={1} flatShading />
       </mesh>
     </>
   );
