7e2edbf9d68197f14bf8ea8f14f5c41d2db0051a
Oskar Kunik <oskar.kunik@gmail.com>
2023-11-16 20:25:54 +0100

    Enable full reload in vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const fullReloadAlways = {
+  handleHotUpdate({ server }) {
+    server.ws.send({ type: "full-reload" })
+    return []
+  },
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), fullReloadAlways],
 })
