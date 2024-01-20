ac0f8969b020cc2b334481b7f6442ea517412de4
Oskar Kunik <oskar.kunik@gmail.com>
2024-01-20 14:53:52 +0100

    Make importing shaders work

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,14 +1,26 @@
 import { OrbitControls } from "@react-three/drei";
-import React from "react";
+import React, { useRef } from "react";
+import vertexShader from "./vertexShader.glsl?raw";
+import fragmentShader from "./fragmentShader.glsl?raw";
 
 export const Experience = () => {
+  const mesh = useRef(null);
   return (
     <>
       <OrbitControls />
       <ambientLight />
-      <mesh>
+      <mesh
+        ref={mesh}
+        position={[0, 0, 0]}
+        rotation={[-Math.PI / 2, 0, 0]}
+        scale={1.5}
+      >
         <icosahedronGeometry args={[4, 30]} />
-        <meshStandardMaterial wireframe color="#ffffff" />
+        <shaderMaterial
+          fragmentShader={fragmentShader}
+          vertexShader={vertexShader}
+          wireframe
+        />
       </mesh>
     </>
   );
