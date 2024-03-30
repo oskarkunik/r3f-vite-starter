fd7bbfd3d695be88aeb65318573cf73b991deac0
Oskar Kunik <oskar.kunik@gmail.com>
2024-03-30 12:23:30 +0100

    Add absolute paths aliases in vite and ts configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+    // vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+    },
+  },
+});
