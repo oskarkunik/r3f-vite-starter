46a3e387a682e63857bff7e5a0452c98dc522dc8
Oskar Kunik <oskar.kunik@gmail.com>
2024-03-30 09:44:43 +0100

    Set dark background

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import React from "react";
 function App() {
   return (
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+      <color attach="background" args={["#1c1e28"]} />
       <Experience />
     </Canvas>
   );
