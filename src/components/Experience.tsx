41e0e8f5f26674a79e12e3b698a7f7d5ab75492f
Oskar Kunik <oskar.kunik@gmail.com>
2024-04-16 20:52:39 +0200

    Create planes for player and enemies

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,8 +5,12 @@ export const Experience = () => {
 
   return (
     <group>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[-4, 0, 0]}>
+        <planeGeometry args={[4, 1]} />
+        <meshNormalMaterial />
+      </mesh>
       <mesh rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[5, 1]} />
+        <planeGeometry args={[2, 1]} />
         <meshNormalMaterial />
       </mesh>
       <ambientLight />
