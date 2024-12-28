4e954815e3446a55a7c3b9acc8a36944c6069e6d
Oskar Kunik <oskar.kunik@gmail.com>
2024-12-28 20:08:41 +0100

    Enable Leva example controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { useControls } from "leva";
 import React from "react";
 
 function App() {
+  const { exampleValue } = useControls({ exampleValue: "#ececec" });
+
   return (
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+      <color attach="background" args={[exampleValue]} />
       <Experience />
     </Canvas>
   );
