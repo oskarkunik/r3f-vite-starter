48e3b5ff56b102af18e204146f5e8349c4484642
Oskar Kunik <oskar.kunik@gmail.com>
2025-02-02 16:45:25 +0100

    Generate circular segments

--- src/components/CircleClock.tsx
+++ src/components/CircleClock.tsx
@@ -2,30 +2,64 @@ import { useControls } from 'leva';
 import React from "react";
 
 export const CircleClock = () => {
-  const MAX_SEGMENTS = 32;
+  const MAX_VERTICLES_IN_CIRCLE = 32;
 
-  const { segments, wireframe } = useControls({
+  const { segments, wireframe, gap, resolution } = useControls({
     segments: {
-      value: MAX_SEGMENTS,
+      value: 2,
       min: 2,
-      max: MAX_SEGMENTS,
-      step: 2,
+      max: 12,
+      step: 1,
     },
-    wireframe: true
+    gap: {
+      value: .2,
+      max: 1,
+      min: 0,
+    },
+    resolution: {
+      value: MAX_VERTICLES_IN_CIRCLE,
+      step: 1,
+      max: 128,
+      min: 8,
+    },
+    wireframe: true,
   });
 
-  const length = () => {
-    const segmentLength = Math.PI * 2 / MAX_SEGMENTS;
-    const length = segments * segmentLength;
-    return length;
+
+  const calculateGap = () => {
+    const gapLength = Math.PI * 2 / resolution * gap;
+    console.log(gapLength)
+    return gapLength;
+  }
+
+
+  const generatePart = (segment: number) => {
+    const length = ((Math.PI * 2) / segments) - calculateGap();
+    const verts = Math.floor(resolution / segments);
+    const startAngle = (length + calculateGap()) * segment;
+
+    return {
+      verts,
+      startAngle,
+      length
+    }
   }
 
   return (
     <>
-      <mesh>
-        <ringGeometry args={[1, 1.4, segments, 1, 0, length()]} />
-        <meshStandardMaterial color="orange" wireframe={wireframe} />
-      </mesh>
+    {
+      Array(segments).fill(null).map((_, i) => {
+        const { verts, startAngle, length } = generatePart(i);
+        return (
+          <mesh>
+            <ringGeometry
+              args={[1, 1.4, verts, 1, startAngle, length]}
+            />
+            <meshStandardMaterial color="orange" wireframe={wireframe} />
+          </mesh>
+        );
+      })
+    }
     </>
   );
 };
